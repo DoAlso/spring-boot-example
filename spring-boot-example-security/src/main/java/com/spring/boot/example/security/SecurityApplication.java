3893e56a9410523510e2db2bbbb6cd1e60dd627e
huyaxi <hyx199170@163.com>
2018-12-26 18:05:34 +0800

    新增权限模块

---
+++ spring-boot-example-security/src/main/java/com/spring/boot/example/security/SecurityApplication.java
@@ -0,0 +1,17 @@
+package com.spring.boot.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName SecurityApplication
+ * @Description TODO
+ * @Author Administrator
+ * @DATE 2018/12/26 10:31
+ */
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class,args);
+    }
+}
